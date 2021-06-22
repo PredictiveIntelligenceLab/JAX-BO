40a72c4a382e74e2d9eb07864be1847a05efe9bd
Mohamed Aziz Bhouri <mohamedazizbhouri@gmail.com>
2021-06-22 12:20:09 -0400

    Update optimizers.py

--- jaxbo/optimizers.py
+++ jaxbo/optimizers.py
@@ -1,7 +1,13 @@
 from scipy.optimize import minimize
 
-def minimize_lbfgs(objective, x0, bnds = None):
+def minimize_lbfgs(objective, x0, verbose = False, bnds = None):
+    if verbose:
+        def callback_fn(params):
+            print("Loss: {}".format(objective(params)[0]))
+    else:
+        callback_fn = None
+        
     result = minimize(objective, x0, jac=True,
                       method='L-BFGS-B', bounds = bnds,
-                      callback=None)
+                      callback=callback_fn)
     return result.x, result.fun
